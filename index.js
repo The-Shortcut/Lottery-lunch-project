2aa117dff5e395d858b5b059d09736315216519e
Arifa <arifabegam.mujawar@gmail.com>
2020-08-31 15:24:27 +0300

    generateEmails, addParticipants end points working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const { Client } = require('pg');
-const models = require('./models')
+const express = require("express");
+const { Client } = require("pg");
+const models = require("./models");
+const cors = require("cors");
 // require('dotenv').config()
 
 // const connectionString = process.env.CONNECTION_STRING;
@@ -9,45 +10,54 @@ const models = require('./models')
 // });
 
 var app = express();
-app.use(express.static('build'))
+app.use(express.static("build"));
 app.use(express.urlencoded());
-app.use(express.json()); 
+app.use(express.json());
 //const query=`SELECT email FROM lottery_lunch_lotterylunchparticipants ORDER BY random() limit 2`
-app.set('port', process.env.PORT || 4000);
+app.set("port", process.env.PORT || 4000);
 
+app.use(cors());
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.get("/api/generateEmails", async function (request, response, next) {
+  try {
+    // const connectionResult = await client.connect();
+    // const res = await client.query(query);
+    // console.log('res ',res);
+    const res = await models.getLotteryParticipants();
+    console.log("RES is ", res);
+    response.status(200).send(res);
+  } catch (err) {
+    console.log(err.stack);
+    response.status(400).send(err);
+  }
 });
 
-app.get('/api/generateEmails', async function(request, response, next) {
-    try {
-        // const connectionResult = await client.connect();
-        // const res = await client.query(query);
-        // console.log('res ',res);
-      const res = await models.getLotteryParticipants()
-        response.status(200).send(res.rows)
-    } catch (err) {
-        console.log(err.stack);
-        response.status(400).send(err);
-    } 
-});
-
-
-app.post('/api/addLotteryParticipants', async function(request, response, next) {
-    console.log('body is ', request.body)
-    try {
-      
-      const res = await models.addLotteryParticipant(request.body)
-        response.status(200).send(res)
-    } catch (err) {
-        console.log(err.stack);
-        response.status(400).send(err);
-    } 
+app.post("/api/addLotteryParticipants", async function (
+  request,
+  response,
+  next
+) {
+  console.log("body is ", request.body);
+  try {
+    const res = await models.addLotteryParticipant(request.body);
+    response.status(200).send(res);
+  } catch (err) {
+    if (err.stack.includes("password authentication failed for user")) {
+      console.log("problem with password");
+      response.status(500).send(err);
+    } else {
+      console.log(err.stack);
+      response.status(400).send(err);
+    }
+  }
 });
 
 app.listen(4000, function () {
-    console.log('Server is running.. on Port 4000');
-});1
+  console.log("Server is running.. on Port 4000");
+});
+1;
--- models.js
+++ models.js
@@ -1,46 +1,43 @@
-const { Client } = require('pg');
-require('dotenv').config()
+require("dotenv").config();
+
+const pg = require("pg");
 
 const connectionString = process.env.CONNECTION_STRING;
-const client = new Client({
-    connectionString: connectionString
+const pool = new pg.Pool({
+  connectionString: connectionString,
 });
 
+const getLotteryParticipants = async () => {
+  const query =
+    "SELECT email FROM lottery_lunch_lotterylunchparticipants ORDER BY random() limit 2";
 
-const getLotteryParticipants = async ()=> {
-    const query='SELECT email FROM lottery_lunch_lotterylunchparticipants ORDER BY random() limit 2'
-    
-        try{
-        
-        const connectionResult = await client.connect();
-        const res = await client.query(query);
-        console.log('res ',res);
-        return res;
-    }catch(er){
-        console.log(er)
-    }
-        finally {
-            client.end();
-        }
-    
-}
+  try {
+    const client = await pool.connect();
+    const res = await client.query(query);
+    console.log("res ", res.rows);
+    client.release(true);
 
+    return res.rows;
+  } catch (er) {
+    console.log(er);
+  }
+};
 
 const addLotteryParticipant = async (body) => {
-    const {email,first_name, last_name, is_active, join_at} = body;   
-    const query = `INSERT INTO lottery_lunch_lotterylunchparticipants (email, first_name, last_name, is_active, join_at) VALUES ('${email}','${first_name}','${last_name}','${is_active}','${join_at}')`;
-    try{
-        await client.connect();
-        console.log('query is ', query)
-        const res = await client.query(query);
-        console.log('res', res)
-        return res;
+  const { email, first_name, last_name, is_active, join_at } = body;
+  const query = `INSERT INTO lottery_lunch_lotterylunchparticipants (email, first_name, last_name, is_active, join_at) VALUES ('${email}','${first_name}','${last_name}','${is_active}','${join_at}')`;
+  
+    const client = await pool.connect();
 
-    }catch(er){
-        return console.log(er)
-    }
-        finally {
-           return client.end();
-        }
-}
-module.exports = { getLotteryParticipants, addLotteryParticipant}
+    console.log("connection is ", client);
+    const res = await client.query(query);
+    console.log("res", res);
+    client.release(true);
+
+    return res;
+ 
+};
+
+
+
+module.exports = { getLotteryParticipants, addLotteryParticipant };
